8fd889cf7fe8653f54106ecd2853bd0fb1f8ea12
Garavirod <rodrigogarciaavila26@gmail.com>
2022-06-19 20:12:38 -0500

    sidebar section styles was added but is not finished yet

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import { Avatar } from '@mui/material'
+import './Sidebar.css'
 function Sidebar() {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+        <div className="sidebar__top">
+            <img src="" alt="" />
+            <Avatar className='sidebar__avatar'/>
+            <h2>Rodrigo García Ávila</h2>
+            <h4>me@gmail.com</h4>            
+        </div>
+        <div className="sidebar__stats">
+            <div className="sidebar__stat">
+                <p>Who viewd you?</p>
+                <p className='sidebar__statNumber'>2,124</p>
+            </div>
+            <div className="diebar__stat">
+                 <p>Who viewd you?</p>
+                <p className='sidebar__statNumber'>2,124</p>
+            </div>
+        </div>
+        <div className="sidebar__bottom">
+            <p>Recent</p>
+        </div>
+    </div>
   )
 }
 
